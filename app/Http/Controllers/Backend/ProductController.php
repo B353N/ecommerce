faee7d890a658d7985dca2b964cfcc74635300f6
B353N <r.slavov.work@gmail.com>
2023-06-05 14:52:33 +0200

    modified:   app/Http/Controllers/Backend/ProductController.php

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -122,7 +122,10 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return response(['status' => 'success', 'message' => 'Product Deleted Successfully']);
     }
 
     /**
